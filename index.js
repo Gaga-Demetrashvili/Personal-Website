5e5c9b9872a4439224980ff2ff35865ed43dbfc8
Vagabond <74795182+Vagabond169@users.noreply.github.com>
2021-08-10 00:26:03 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+$("button").click(function(){
+    $(".sec").toggleClass("sec2");
+    $("table").toggleClass("table2");
+    $("hr").toggleClass("hr2");
+    $("th").toggleClass("th2");
+    // $("p,h2,h3,h4").toggleClass("night-mode");
+    $("body").toggleClass("night-mode");
+    if ($("section").hasClass("sec2")) {
+        $("button").html("<i class='far fa-2x fa-lightbulb'></i>");
+    }else {
+        $("button").html("<i class='fas fa-2x fa-lightbulb'></i>")
+    }
+})
